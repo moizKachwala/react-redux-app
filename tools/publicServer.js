f696b3f439e68886f587fda9aae0ea744e6e5c1a
Moiz.Kachwala <moizk@hotmail.com>
2018-06-28 14:25:27 +0530

    added favicon

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import favicon from 'serve-favicon';
 
 /*eslint-disable no-console */
 
@@ -8,6 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('public'));
+app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
